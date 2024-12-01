e845d917e1b101c79c608890408be57d535cc882
Salim Khan <Khan.salim.ali@gmail.com>
2024-12-01 12:44:24 -0500

    Day 01, light clean up

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,70 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
+    List<int> FirstList = new List<int>();
+    List<int> SecondList = new List<int>();
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
+
+        foreach (string Line in _input)
+        {
+            string[] TempSplit = Line.Split("   ");
+            FirstList.Add(int.Parse(TempSplit[0]));
+            SecondList.Add(int.Parse(TempSplit[1]));
+        }
+        
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public string Part1()
+    {
+        int sum = 0;
+        FirstList.Sort();
+        SecondList.Sort();
+        for (int i = 0; i < FirstList.Count; i++)
+        {
+            int difference = SecondList[i] - FirstList[i];
+            int distance = difference>0 ? difference : difference*-1;
+            sum += distance;
+        }
+        
+        return sum.ToString();
+    }
+    
+    public string Part2()
+    {
+        int sum = 0;
+       Dictionary<int, int> Occurances = new Dictionary<int, int>();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+       foreach (int location in SecondList)
+       {
+           if(!Occurances.ContainsKey(location))
+           {
+               Occurances.Add(location, 1);
+           }
+           else
+           {
+               Occurances[location]++;
+           }
+       }
+
+       foreach (int location in FirstList)
+       {
+           if (!Occurances.ContainsKey(location))
+           {
+               continue;
+           }
+           else
+           {
+               sum += Occurances[location]*location;
+           }
+       }
+       
+       return sum.ToString();
+    }
+    public override ValueTask<string> Solve_1() => new(Part1());
+
+    public override ValueTask<string> Solve_2() => new(Part2());
 }
